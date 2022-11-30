39cde8fd43875273fdb91d3bfd852338ebeaa8e4
Satoshi <48849803+ShSato4JPN@users.noreply.github.com>
2022-11-30 22:50:43 +0900

    Feature/#27 change the directory for container components (#40)
    * feat: change the directory for container components
    
    * feat: raised next version from 13.0.0 to 13.0.5
    - upgraded the version of next package
    - change directory structure
    - it automatically changed the value of tsconfig.json
    
    close #27
    
    * fix:  add "use client" to Welcome component
    
    close #27

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Welcome from "components/Welcome";
+
+function Page() {
+  return (
+    <>
+      <title>welcome next.js v13</title>
+      <Welcome />
+    </>
+  );
+}
+
+export default Page;
