c468ca06bb791be77d148b059c91b75c347b2ecd
Satoshi <48849803+ShSato4JPN@users.noreply.github.com>
2023-03-01 23:53:02 +0900

    feat: create api of get blog list (#75)
    * feat: create api of get blog list
    
    Close #74
    
    * fix: add no store

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,26 @@
-function Page() {
+import { GetEntriesData } from "app/api/entries/route";
+import queryString from "query-string";
+
+async function getEntries(): Promise<GetEntriesData> {
+  const res = await fetch(
+    queryString.stringifyUrl({
+      url: `${process.env.URL}/api/entries`,
+      query: {
+        skip: 0,
+        limit: 5,
+        order: "-fields.publishedAt",
+      },
+    }),
+    { cache: "no-store" }
+  );
+
+  return res.json();
+}
+
+async function Page() {
+  const data = await getEntries();
+  console.dir(data.items);
+
   return <div>aaa</div>;
 }
 
