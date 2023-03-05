23cc6135413cce7bb1fc4ef735edff1024400a49
ShSato4JPN <shsato4.private.1221@gmail.com>
2023-03-06 00:27:54 +0900

    fix: remove swr settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
-"use client";
 import "./globals.scss";
-import { SWRConfig } from "swr";
-import { ReactNode } from "react";
-import fetcher from "libs/fetcher";
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="ja">
-      <body>
-        <SWRConfig value={{ fetcher }}>{children}</SWRConfig>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
