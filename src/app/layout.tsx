aae9ce717a233516282eba3f2b0badc0db36f633
ShSato4JPN <shsato4.private.1221@gmail.com>
2023-03-06 23:41:00 +0900

    feat: add global swr
    Close #79

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
+"use client";
 import "./globals.scss";
+import { SWRConfig } from "swr";
+import { ReactNode } from "react";
+import fetcher from "libs/fetcehr";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body>
+        <SWRConfig value={{ fetcher }}>{children}</SWRConfig>
+      </body>
     </html>
   );
 }
